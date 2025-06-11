f7167a9c8d951ae5951664ab066ebe3ecef3e829
tanzina1j <tanziinashrity59@gmail.com>
2025-06-12 02:05:27 +0600

    my first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <section>
+        <header class="header-section">
+				<div class="container">
+					<div class="header-wrapper">
+						<div class="logo-menu">
+							<a href="index.html" class="logo">
+								<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTddF2NIBS8vJRNDxVN1B1NTZtEh7Y6lNCNrdpPL7C3AJLjYTGX" alt="main-logo"/>
+							</a>
+						</div>
+						<ul class="main-menu">
+							<li class="active">
+								<a href="#0">home</a>
+							</li>
+							<li>
+								<a href="#0">About</a>
+							</li>
+							<li>
+								<a href="#0">Services</a>
+							</li>
+							<li>
+								<a href="#0">Team</a>
+							</li>
+							<li>
+								<a href="#0">Blog</a>
+							</li>
+							<li>
+								<a href="#0">Pages</a>
+							</li>
+							<li>
+								<a href="#0">Shop</a>
+							</li>
+							<li>
+								<a href="contact.html">Contact Us </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</header>
+    </section>
+  )
+}
+
+export default Header
